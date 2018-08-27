82a7d797452b9b8056cc4d1cb79c37897ce753d1
okinaga <okinagagoushi54@gmail.com>
2018-08-27 21:21:03 +0900

    8月27日(月)

---
+++ TestProgect01/src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		// Hello World! の出力
+		System.out.println("Hello World!");
+		System.out.println("こんにちは！！");
+		System.out.println(123);
+		System.out.println(0);
+		System.out.println("例外が発生しました。");
+		System.out.println("例外が");
+		System.out.println("発生しました。");
+		System.out.println("例外が\r\n発生しました。");
+		
+	}
+
+}
